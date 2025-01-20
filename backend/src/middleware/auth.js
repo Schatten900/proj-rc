163087d11b72473c6fb07b54ab88d6bdf2ba45b8
Luke0133 <lucahmeg@gmail.com>
2025-01-20 00:37:31 -0300

    Figuras do Relatório

--- backend/src/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-app.checkAuth('/api/check-session', (req, res) => {
-        if (req.session.email) {
-            res.json({ loggedIn: true });
-        } else {
-            res.json({ loggedIn: false });
-        }
-    });
